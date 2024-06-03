7ba95c7d15d948027af4df742650a5aeb5bb5bda
thanhhien234 <doanthanhhien01@gmail.com>
2024-06-03 23:40:38 +0900

    use useWindowDimensions

--- Dimensions/App.js
+++ Dimensions/App.js
@@ -1,37 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, useWindowDimensions } from 'react-native';
 
 export default function App() {
-
-  const [dimensions, setDimensions] = useState({
-    window: Dimensions.get('window'),
-  });
-
-  useEffect(() => {
-    const subscription = Dimensions.addEventListener('change', ({ window, screen }) => {
-      setDimensions({ window });
-    });
-    console.log("Dimensions:", dimensions);
-    return () => subscription?.remove();
-  }
-  , [dimensions]);
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
 
   return (
     <View style={styles.container}>
 
       <View style={[styles.box, {
-        width: dimensions.window.width > 500 ? "40%" : "80%",
-        height: dimensions.window.height > 600 ? "50%" : "30%",
+        width: windowWidth > 500 ? "40%" : "80%",
+        height: windowHeight > 600 ? "50%" : "30%",
       }]}>
       </View>
 
     </View>
   );
 }
-
-// const windowWidth = Dimensions.get('window').width;
-// const windowHeight = Dimensions.get('window').height;
-// console.log("width:",windowWidth, " , height:", windowHeight);
 
 const styles = StyleSheet.create({
   container: {
@@ -41,8 +25,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box : {
-    // width: windowWidth > 500 ? "40%" : "80%",
-    // height: windowHeight > 600 ? "50%" : "30%",
     backgroundColor: 'lightblue',
   },
 });
