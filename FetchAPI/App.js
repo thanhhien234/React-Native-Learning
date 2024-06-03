4609fbc1462b73ffdf88c7d0c017077d5f695834
thanhhien234 <doanthanhhien01@gmail.com>
2024-06-04 02:56:54 +0900

    FetchAPI: POST

--- FetchAPI/App.js
+++ FetchAPI/App.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, SafeAreaView, StatusBar, View, FlatList, Text, ActivityIndicator } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar, View, FlatList, Text, ActivityIndicator, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [refreshing, setRefreshing] = useState(false);
+  const [posts, setPosts] = useState([]);  //for GET
+
+  const [loading, setLoading] = useState(true); //loading when waiting data
+  const [refreshing, setRefreshing] = useState(false); //refreshing when pull down
+
+  const [postTitle, setPostTitle] = useState('');  //for POST
+  const [postBody, setPostBody] = useState('');
+  const [isPost, setIsPost] = useState(false);
+
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
@@ -16,9 +22,37 @@ export default function App() {
     setLoading(false);
   }
 
+  const addPost = async () => {
+    setIsPost(true);
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: postTitle,
+          body: postBody,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const newPost = await response.json();
+      setPosts([newPost,...posts]);  //add to the top of the list
+      setPostTitle("");
+      setPostBody("");
+      setIsPost(false);
+    } catch (error) {
+      console.error("Error adding new Pokémon:", error);
+      setError("Failed to add new Pokémon.");
+    }
+    setIsPost(false);
+  };
+
   useEffect(() => {
     fetchData();
-  } , []);
+  }, []);
 
   if (loading) {
     return (
@@ -30,6 +64,26 @@ export default function App() {
   }
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="postTitle"
+          value={postTitle}
+          onChangeText={setPostTitle}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="postBody"
+          value={postBody}
+          onChangeText={setPostBody}
+        />
+        <Button
+          title={isPost ? "Adding..." : "Add Post"}
+          onPress={addPost}
+          disabled={isPost}
+        />
+      </View>
+
       <View style={styles.listContainer}>
         <FlatList
           data={posts}
@@ -48,7 +102,7 @@ export default function App() {
           }}
         />
       </View>
-      
+
     </SafeAreaView>
   );
 }
@@ -78,5 +132,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  inputContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
   },
 });
