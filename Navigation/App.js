9a8fc83e7e2c9d2603f6c955067c1cbacf463d83
thanhhien234 <doanthanhhien01@gmail.com>
2024-06-04 13:43:37 +0900

    Navigation: add option in Navigator

--- Navigation/App.js
+++ Navigation/App.js
@@ -4,6 +4,8 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Pressable,Text } from 'react-native';
+
 import Home from './Screens/Home';
 import Profile from './Screens/Profile';
 
@@ -14,7 +16,27 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Home" component={Home} options={
+            {
+              title: 'Welcome Home',
+              headerStyle: {
+                backgroundColor: '#f4511e',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+              headerRight: () => (
+                <Pressable onPress={() => alert('Menu button pressed')}>
+                  <Text style={{ color: 'white', marginRight: 10 }}>Menu</Text>
+                </Pressable>
+              ),
+              contentStyle : {
+                backgroundColor: "#e8e8e8e"
+              }
+            }
+          
+          }/>
           <Stack.Screen name="Profile" component={Profile} />
         </Stack.Navigator>
       </NavigationContainer>
--- Navigation/Screens/Home.js
+++ Navigation/Screens/Home.js
@@ -4,7 +4,7 @@ export default function Home({  route, navigation }) {
   return (
     <View>
       <Text>Home Screen</Text>
-      <Text>Updated name = {route.params?.result}</Text>   {/* receive "result" from Profile screen */}
+      <Text>Updated name = {route.params?.result}</Text>   
       <Button
         title="Go to Profile"
         onPress={() => navigation.navigate('Profile', {name: 'Thanh Hien'})}  //send "name" to Profile screen
